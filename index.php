4dd55a66d7fc8db197315e2c47b10b6c7402b9ca
Julius Sipos <julius.sipos98@gmail.com>
2023-05-02 21:15:57 +0200

    Added WordPress Template parts

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-	<?php wp_head(); ?>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-  </body>
-  <?php wp_footer(); ?>
-</html>
+<?php get_header(); ?>
+<h1>Hello, world! -- JA SOM INDEX PHP</h1>
+<?php get_footer(); ?>
