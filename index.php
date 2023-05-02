14acae907c6b5087b43f545459ceb3b07610c2d4
Julius Sipos <julius.sipos98@gmail.com>
2023-05-02 21:02:02 +0200

    Implemented Bootstrap 5 layout

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<?php
-
-wp_head();
-
-
-echo 'IT IS WORKING';
-
-
-wp_footer();
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+	<?php wp_head(); ?>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
+  <?php wp_footer(); ?>
+</html>
